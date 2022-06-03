792d6f47f52c60370e12348e5d749481eee5316d
wencheng2 <103392823+wencheng2@users.noreply.github.com>
2022-06-03 17:06:05 +1000

    Updating projects
    Adding first 3 projects

---
+++ Codecademy projects/initials.py
@@ -0,0 +1,13 @@
+#Name = Wen Cheng
+#Fun Fact = I have a dog that looks like an ewok
+
+#Code to print my initials
+print("""
+W   W  CCC 
+W   W C   C
+W   W C
+W W W C
+WW WW C
+WW WW C   C
+W   W  CCC
+""")
---
+++ magic8ball.py
@@ -0,0 +1,45 @@
+#magic8.py
+#This is a Magic 8-Ball program that answers and "Yes" or "No" question with a different fortune
+
+#Import module
+import random
+
+#Set up variables
+name = input("Your name: ")
+ques = input("Your question: ")
+ans = ""
+
+#Create random number variable
+random_num = random.randint(1,9)
+
+#Generate response to question
+if random_num == 1:
+  ans = "Yes - definitely."
+elif random_num == 2:
+  ans =  "It is decidedly so."
+elif random_num == 3:
+  ans =  "Without a doubt."
+elif random_num == 4:
+  ans =  "Reply hazy, try again."
+elif random_num == 5:
+  ans = "Ask again later."
+elif random_num == 6:
+  ans = "Better not tell you now."
+elif random_num == 7:
+  ans = "My sources say no."
+elif random_num == 8:
+  ans = "Outlook not so good."
+elif random_num == 9:
+  ans = "Very doubtful."
+else:
+  ans = "Error"
+
+#Check question was asked
+if len(ques) == 0:
+    print("The Magic 8-Ball cannot provide a fortune unless you ask it something")
+#Output on terminal
+elif name == "":
+  print("You asked: " + ques)
+else:
+  print(name + " asks: " + ques)
+  print("Magic 8-Ball's answer: " + ans)
